40785987e65d56184f1a4c27742461802383123a
Jangseongjin <sungjin5891@gmail.com>
2021-08-08 22:54:59 +0900

    수정 : 설정

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210808.2033';
+const cacheName = 'chirpy-20210808.2254';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
